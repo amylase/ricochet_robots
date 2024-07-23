3f8414b6f1e6db2d22df525acc463eda9af6780d
Shunsuke Ohashi <s.ohashi@monoxer.com>
2024-07-23 21:52:38 +0900

    faster

--- src/solver.rs
+++ src/solver.rs
@@ -1,18 +1,15 @@
 use std::collections::VecDeque;
 
-
 use bitvec::{order::Msb0, bitvec};
-use fnv::FnvHashMap;
 
 use crate::model::{GameMove, GameSpec, GameState};
 
 pub fn solve_bfs(spec: &GameSpec, initial_state: &GameState) -> Vec<GameMove> {
-    let mut back_edge = FnvHashMap::default();
+    let mut back_edge = Vec::new();
     let mut vis = bitvec![u64, Msb0; 0; 1 << 32];
 
     let mut q = VecDeque::new();
     q.push_back(initial_state.clone());
-    back_edge.insert(initial_state.clone(), (GameMove::default(), initial_state.clone()));
     vis.set(initial_state.to_u32() as usize, true);
 
     let mut final_state: Option<GameState> = None;
@@ -23,7 +20,7 @@ pub fn solve_bfs(spec: &GameSpec, initial_state: &GameState) -> Vec<GameMove> {
             if *vis.get(next_state.to_u32() as usize).unwrap() {
                 continue;
             }            
-            back_edge.insert(next_state.clone(), (game_move, current_state.clone()));
+            back_edge.push((next_state.clone(), game_move, current_state.clone()));
             vis.set(next_state.to_u32() as usize, true);
             if spec.is_winning_state(&next_state) {
                 final_state = Some(next_state);
@@ -37,9 +34,13 @@ pub fn solve_bfs(spec: &GameSpec, initial_state: &GameState) -> Vec<GameMove> {
     let mut moves: Vec<GameMove> = vec![];
 
     while state != *initial_state {
-        let (game_move, prev_state) = back_edge.get(&state).unwrap();
-        moves.push(game_move.clone());
-        state = prev_state.clone();
+        for (current_state, game_move, prev_state) in back_edge.iter() {
+            if *current_state == state {
+                moves.push(game_move.clone());
+                state = prev_state.clone();
+                break;
+            }
+        }
     }
     moves.reverse();
     return moves;
